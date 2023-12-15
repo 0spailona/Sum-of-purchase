78d946e2925a7d1ff3b65d88c39d1845daa1cdb7
Daria <n-d-p@mail.ru>
2023-12-15 23:58:00 +0300

    Cart.ts => + methods: sum, get totalAmount, sumWithDiscount, deleteItem; Cart.test.ts => + tests for new methods

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -1,17 +1,55 @@
 import Cart from '../service/Cart';
 import Movie from "../domain/Movie";
 
-test('new card should be empty', () => {
+test('new card should be empty', ():void => {
   const cart = new Cart();
 
   expect(cart.items.length).toBe(0);
 });
 
-test('add new movie to cart', () => {
+test('add new movie to cart', ():void => {
   const cart = new Cart();
   const movie = new Movie(1, '1', 1, 2, 'F', ['e', 'r', 'd'], '123 min', '1');
   cart.add(movie);
   expect(cart.items).toEqual([{
     country: "F", id: 1, name: "1", price: 2, release: 1, slogan: "1", style: ["e", "r", "d",], time: "123 min",
   },])
+})
+
+test('delete item from cart', ():void => {
+  const cart = new Cart();
+  const movie1 = new Movie(1, '1', 1, 2, 'F', ['e', 'r', 'd'], '123 min', '1');
+  const movie2 = new Movie(2, '1', 1, 2, 'F', ['e', 'r', 'd'], '123 min', '1');
+  const movie3 = new Movie(3, '1', 1, 2, 'F', ['e', 'r', 'd'], '123 min', '1');
+  cart.add(movie1);
+  cart.add(movie2);
+  cart.add(movie3);
+  cart.deleteItem(2);
+  expect(cart.items).toEqual([{
+    country: "F", id: 1, name: "1", price: 2, release: 1, slogan: "1", style: ["e", "r", "d",], time: "123 min",
+  }, {country: "F", id: 3, name: "1", price: 2, release: 1, slogan: "1", style: ["e", "r", "d",], time: "123 min",}])
+})
+
+test('to get total amount', ():void => {
+  const cart = new Cart();
+  const movie1 = new Movie(1, '1', 1, 2, 'F', ['e', 'r', 'd'], '123 min', '1');
+  const movie2 = new Movie(2, '1', 1, 2, 'F', ['e', 'r', 'd'], '123 min', '1');
+  const movie3 = new Movie(3, '1', 1, 2, 'F', ['e', 'r', 'd'], '123 min', '1');
+  cart.add(movie1);
+  cart.add(movie2);
+  cart.add(movie3);
+  expect(cart.totalAmount).toBe(6)
+})
+
+
+test('to get total amount with discount', ():void => {
+  const cart = new Cart();
+  const discount: number = 5;
+  const movie1 = new Movie(1, '1', 1, 2, 'F', ['e', 'r', 'd'], '123 min', '1');
+  const movie2 = new Movie(2, '1', 1, 2, 'F', ['e', 'r', 'd'], '123 min', '1');
+  const movie3 = new Movie(3, '1', 1, 2, 'F', ['e', 'r', 'd'], '123 min', '1');
+  cart.add(movie1);
+  cart.add(movie2);
+  cart.add(movie3);
+  expect(cart.sumWithDiscount(discount)).toBe(5.7)
 })
--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -1,13 +1,33 @@
 import Buyable from '../domain/Buyable';
 
 export default class Cart {
-    private _items: Buyable[] = [];
 
-    add(item: Buyable): void {
-        this._items.push(item);
-    }
+  private _totalAmount = 0
+  private _items: Buyable[] = [];
 
-    get items(): Buyable[] {
-        return [...this._items]; 
-    }
+  add(item: Buyable): void {
+    this._items.push(item);
+    this.sum();
+  }
+
+  get items(): Buyable[] {
+    return [...this._items];
+  }
+
+  sum(): void {
+    this._totalAmount = [...this.items].reduce((sum, item) =>
+      sum + item.price, 0)
+  }
+
+  get totalAmount(): number {
+    return this._totalAmount;
+  }
+
+  sumWithDiscount(discount: number): number {
+    return this._totalAmount - (this._totalAmount * discount / 100)
+  }
+
+  deleteItem(id: number): void {
+    [...this._items] = [...this._items].filter(item => item.id !== id)
+  }
 }
